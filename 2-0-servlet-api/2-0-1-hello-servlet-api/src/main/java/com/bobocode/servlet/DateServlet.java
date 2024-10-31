7ec32cb833a7e8628e9c70c39e5be4b38a6cd2a5
Brian Vega <BrianVega01@outlook.es>
2024-10-31 17:56:09 -0600

    DateServletTests

---
+++ 2-0-servlet-api/2-0-1-hello-servlet-api/src/main/java/com/bobocode/servlet/DateServlet.java
@@ -0,0 +1,22 @@
+package com.bobocode.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+@WebServlet("/date")
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("text/html");
+        writer.println(LocalDateTime.now());
+    }
+}
